19d82168feb7e439d8586a919beee2996b6abef6
RuixZh <38558445+RuixZh@users.noreply.github.com>
2023-10-30 15:34:24 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -11,65 +11,64 @@ import yaml
 from dotmap import DotMap
 import pprint
 import numpy as np
-from modules.Visual_Prompt import visual_prompt
-from utils.Augmentation import get_augmentation
 import torch
+from utils.tools import *
+from modules import FMCLIP
 
-class TextCLIP(nn.Module):
-    def __init__(self, model):
-        super(TextCLIP, self).__init__()
-        self.model = model
 
-    def forward(self, text):
-        return self.model.encode_text(text)
-
-class ImageCLIP(nn.Module):
-    def __init__(self, model):
-        super(ImageCLIP, self).__init__()
-        self.model = model
-
-    def forward(self, image):
-        return self.model.encode_image(image)
-
-def validate(epoch, val_loader, classes, device, model, fusion_model, config):
+def validate(epoch, val_loader, item_des, score_des, device, model, fm_model, config):
     model.eval()
-    fusion_model.eval()
+    fm_model.eval()
     num = 0
-    corr_1 = 0
-    corr_3 = 0
-    corr_5 = 0
+    item_corr_1, score_corr_1 = 0, 0
+    item_corr_3, score_corr_3 = 0, 0
 
     with torch.no_grad():
-        text_inputs = classes.to(device)
-        text_features = model.encode_text(text_inputs)
-        for iii, (image, class_id) in enumerate(tqdm(val_loader)):
-            image = image.view((-1, config.data.num_segments, 3) + image.size()[-2:])
-            b, t, c, h, w = image.size()
-            class_id = class_id.to(device)
-            image_input = image.to(device).view(-1, c, h, w)
-            image_features = model.encode_image(image_input).view(b, t, -1)
-            image_features = fusion_model(image_features)
-            image_features /= image_features.norm(dim=-1, keepdim=True)
-            text_features /= text_features.norm(dim=-1, keepdim=True)
-            similarity = (torch.mm(100.0 * image_features,text_features.T))
-            similarity = similarity.view(b, -1).softmax(dim=-1)
-            # similarity = similarity.mean(dim=1, keepdim=False)
-            values_1, indices_1 = similarity.topk(1, dim=-1)
-            values_3, indices_3 = similarity.topk(3, dim=-1)
-            values_5, indices_5 = similarity.topk(5, dim=-1)
+        score_des_emb = model.encode_text(score_des)
+        item_des_emb = model.encode_text(item_des)
+        for iii, (frames, texts, item_label, score_label) in enumerate(tqdm(val_loader)):
+
+            frames = frames.view((-1, config.data.num_segments, 3) + frames.size()[-2:])
+            b,t,c,h,w = frames.size()
+            frames= frames.to(device).view(-1,c,h,w)
+            frame_emb = model.encode_image(frames)
+            frame_emb = frame_emb.view(b,t,-1)
+            texts = texts.view(-1, 77).to(device)
+            text_emb = model.encode_text(texts)  #.view(b, 5, -1)
+
+            logit_scale = 1.0
+            score_truth = torch.tensor(np.zeros(shape=(len(score_label), score_des_emb.shape[0])), dtype=frame_emb.dtype, device=device) # (bs, bs)
+            item_truth = torch.tensor(np.zeros(shape=(len(item_label), item_des_emb.shape[0])), dtype=frame_emb.dtype, device=device) # (bs, bs)
+
+            v2i_sim, v2s_sim, loss = fm_model(frame_emb, text_emb, item_des_emb, score_des_emb, logit_scale, item_truth, score_truth)
+            # print(v2i_sim, v2s_sim)
+            v2i_sim = v2i_sim.view(b, -1).softmax(dim=-1)
+            v2s_sim = v2s_sim.view(b, -1).softmax(dim=-1)
+
+            item_1, idx_item_1 = v2i_sim.topk(1, dim=-1)
+            item_3, idx_item_3 = v2i_sim.topk(3, dim=-1)
+
+            score_1, idx_score_1 = v2s_sim.topk(1, dim=-1)
+            score_3, idx_score_3 = v2s_sim.topk(3, dim=-1)
+
             num += b
             for i in range(b):
-                if indices_1[i] == class_id[i]:
-                    corr_1 += 1
-                if class_id[i] in indices_3[i]:
-                    corr_3 += 1
-                if class_id[i] in indices_5[i]:
-                    corr_5 += 1
-    top1 = float(corr_1) / num * 100
-    top3 = float(corr_3) / num * 100
-    top5 = float(corr_5) / num * 100
-    print('Epoch: [{}/{}]: Top1: {:.4f}%, Top3: {:.4f}%, Top5: {:.4f}%'.format(epoch, config.solver.epochs, top1, top3, top5))
-    return top1, top3, top5
+                if idx_item_1[i] == item_label[i]:
+                    item_corr_1 += 1
+                if item_label[i] in idx_item_3[i]:
+                    item_corr_3 += 1
+                if idx_score_1[i] == score_label[i]:
+                    score_corr_1 += 1
+                if score_label[i] in idx_score_3[i]:
+                    score_corr_3 += 1
+    item_top1 = float(item_corr_1) / num * 100
+    item_top3 = float(item_corr_3) / num * 100
+    score_top1 = float(score_corr_1) / num * 100
+    score_top3 = float(score_corr_3) / num * 100
+    print('Epoch: [{}/{}]:'.format(epoch, config.solver.epochs))
+    print('Test Item: Top1: {:.4f}%, Top3: {:.4f}%'.format(item_top1, item_top3))
+    print('Test Score: Top1: {:.4f}%, Top3: {:.4f}%'.format(score_top1, score_top3))
+    return item_top1, item_top3, score_top1, score_top3
 
 def main():
     global args, best_prec1
@@ -92,25 +91,10 @@ def main():
                                                    T=config.data.num_segments, dropout=config.network.drop_out,
                                                    emb_dropout=config.network.emb_dropout)  # Must set jit=False for training  ViT-B/32
 
-
-    fusion_model = visual_prompt(config.network.sim_header, clip_state_dict, config.data.num_segments)
-
-    model_text = TextCLIP(model)
-    model_image = ImageCLIP(model)
-
-    model_text = torch.nn.DataParallel(model_text).cuda()
-    model_image = torch.nn.DataParallel(model_image).cuda()
-    fusion_model = torch.nn.DataParallel(fusion_model).cuda()
+    fm_model = FMCLIP(config.network.sim_header, clip_state_dict,config.data.num_segments).to(device)
 
     val_data = VideoDataset(config.data.val_list, config.data.label_list, config.data.num_segments, config.data.input_size)
     val_loader = DataLoader(val_data,batch_size=config.data.batch_size,num_workers=config.data.workers,shuffle=False,pin_memory=False,drop_last=False)
-
-    if device == "cpu":
-        model_text.float()
-        model_image.float()
-    else:
-        clip.model.convert_weights(model_text)
-        clip.model.convert_weights(model_image)
 
     start_epoch = config.solver.start_epoch
 
@@ -119,13 +103,14 @@ def main():
             print(("=> loading checkpoint '{}'".format(config.pretrain)))
             checkpoint = torch.load(config.pretrain)
             model.load_state_dict(checkpoint['model_state_dict'])
-            fusion_model.load_state_dict(checkpoint['fusion_model_state_dict'])
+            fm_model.load_state_dict(checkpoint['fusion_model_state_dict'])
             del checkpoint
         else:
             print(("=> no checkpoint found at '{}'".format(config.pretrain)))
+    score_des = val_data.score_des.to(device)
+    item_des = val_data.item_des.to(device)
+    item_top1, item_top3, score_top1, score_top3 = validate(start_epoch, val_loader, item_des, score_des, device, model, fm_model, config)
 
-    best_prec1 = 0.0
-    prec1, prec3, prec5 = validate(start_epoch,val_loader, val_data.classes, device, model,fusion_model, config)
 
 if __name__ == '__main__':
     main()
--- train.py
+++ train.py
@@ -8,8 +8,7 @@ import shutil
 from pathlib import Path
 import yaml
 import pprint
-from modules.Visual_Prompt import visual_prompt
-from utils.KLLoss import KLLoss
+from modules import FMCLIP
 from test import validate
 from utils.solver import _optimizer, _lr_scheduler
 from utils.tools import *
@@ -19,24 +18,24 @@ import warnings
 import random
 import numpy as np
 import clip
+from fvcore.nn import FlopCountAnalysis, parameter_count_table
 warnings.filterwarnings('ignore')
 
-
-class TextCLIP(nn.Module):
-    def __init__(self, model) :
-        super(TextCLIP, self).__init__()
-        self.model = model
-
-    def forward(self,text):
-        return self.model.encode_text(text)
-
-class ImageCLIP(nn.Module):
-    def __init__(self, model) :
-        super(ImageCLIP, self).__init__()
-        self.model = model
-
-    def forward(self,image):
-        return self.model.encode_image(image)
+# class TextCLIP(nn.Module):
+#     def __init__(self, model) :
+#         super(TextCLIP, self).__init__()
+#         self.model = model
+#
+#     def forward(self,text):
+#         return self.model.encode_text(text)
+#
+# class ImageCLIP(nn.Module):
+#     def __init__(self, model) :
+#         super(ImageCLIP, self).__init__()
+#         self.model = model
+#
+#     def forward(self,image):
+#         return self.model.encode_image(image)
 
 def main():
     global args, best_prec1
@@ -62,66 +61,83 @@ def main():
     torch.backends.cudnn.deterministic = True
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    model, clip_state_dict = clip.load(config.network.arch, device=device,jit=False, tsm=config.network.tsm, T=config.data.num_segments,dropout=config.network.drop_out, emb_dropout=config.network.emb_dropout,pretrain=config.network.init, joint = config.network.joint) #Must set jit=False for training  ViT-B/32
+    model, clip_state_dict = clip.load(config.network.arch, device=device, jit=False, tsm=config.network.tsm, T=config.data.num_segments,dropout=config.network.drop_out, emb_dropout=config.network.emb_dropout,pretrain=config.network.init, joint = config.network.joint) #Must set jit=False for training  ViT-B/32
 
-    fusion_model = visual_prompt(config.network.sim_header, clip_state_dict,config.data.num_segments)
-    model_text = TextCLIP(model)
-    model_image = ImageCLIP(model)
-    model_text = torch.nn.DataParallel(model_text).to(device)
-    model_image = torch.nn.DataParallel(model_image).to(device)
-    fusion_model = torch.nn.DataParallel(fusion_model).to(device)
-    # wandb.watch(model)
-    # wandb.watch(fusion_model)
+    fm_model = FMCLIP(config.network.sim_header, clip_state_dict,config.data.num_segments).to(device)
+    # fm_model = torch.nn.DataParallel(fm_model).to(device)
+    # wandb.watch(fm_model)
     train_data = VideoDataset(config.data.train_list, config.data.label_list, config.data.num_segments, config.data.input_size, isTraining=True)
     train_loader = DataLoader(train_data, batch_size=config.data.batch_size, num_workers=config.data.workers, shuffle=True, pin_memory=False, drop_last=True)
     val_data = VideoDataset(config.data.val_list, config.data.label_list, config.data.num_segments, config.data.input_size)
     val_loader = DataLoader(val_data, batch_size=config.data.batch_size, num_workers=config.data.workers, shuffle=False, pin_memory=False, drop_last=False)
-    loss_img = KLLoss()
-    loss_txt = KLLoss()
 
     start_epoch = config.solver.start_epoch
 
-    optimizer = _optimizer(config, model, fusion_model)
+    optimizer = _optimizer(config, model, fm_model)
     lr_scheduler = _lr_scheduler(config, optimizer)
 
-    best_prec1, best_prec5 = 0.0, 0.0
+    # i1 = torch.randint(100, (1, 77)).to(device)
+    # i11 = torch.randint(100, (4, 77)).to(device)
+    # i12 = torch.randint(100, (12, 77)).to(device)
+    #
+    # i2 = torch.randn(8, 3, 224, 224).to(device)
+    # i31 = torch.randn(1, 8, clip_state_dict["text_projection"].shape[1]).to(device)
+    # i32 = torch.randn(1, clip_state_dict["text_projection"].shape[1]).to(device)
+    #
+    # i33 = torch.randn(4, clip_state_dict["text_projection"].shape[1]).to(device)
+    # i34 = torch.randn(12, clip_state_dict["text_projection"].shape[1]).to(device)
+    #
+    # i35 = torch.tensor(0., device=device)
+    # i36 = torch.randn(1, 4).to(device)
+    # i37 = torch.randn(1, 12).to(device)
+    #
+    # text_enc = TextCLIP(model).to(device)
+    # img_enc = ImageCLIP(model).to(device)
+    # flops = FlopCountAnalysis(text_enc, (i1)).total() + FlopCountAnalysis(text_enc, (i11)).total()+FlopCountAnalysis(text_enc, (i12)).total()
+    # flops2 = FlopCountAnalysis(img_enc, (i2)).total()
+    # flops3 = FlopCountAnalysis(fm_model, (i31, i32, i33, i34, i35, i36, i37)).total()
+    #
+    # print("FLOPs: ", (flops+flops2+flops3)/1e9)
+    # print(parameter_count_table(model))
+    # print('-' * 80)
+    # n_parameters = sum(p.numel() for p in fm_model.parameters() if p.requires_grad) + sum(p.numel() for p in model.parameters() if p.requires_grad)
+    # print('# params:', n_parameters)
+    # print('-' * 80)
 
-    step_count = 0
-    loss_max = 100
+    bi1, bi3, bs1, bs3 = 0.0, 0.0, 0.0, 0.0
+
+    score_des = train_data.score_des.to(device)
+    item_des = train_data.item_des.to(device)
 
     for epoch in range(start_epoch, config.solver.epochs):
-        model_image.train()
-        model_text.train()
-        fusion_model.train()
+        model.train()
+        fm_model.train()
         total_loss = 0.0
-        for kkk,(images,list_id) in enumerate(tqdm(train_loader)):
+        for kkk, (frames, texts, item_label, score_label) in enumerate(tqdm(train_loader)):
             if config.solver.type != 'monitor':
                 if (kkk+1) == 1 or (kkk+1) % 10 == 0:
                     lr_scheduler.step(epoch + kkk / len(train_loader))
             optimizer.zero_grad()
 
-            images = images.view((-1, config.data.num_segments, 3) + images.size()[-2:])
-            b,t,c,h,w = images.size()
-            # text_id = np.random.randint(train_data.num_text_aug, size=len(list_id))
-            # texts = torch.stack([train_data.text_dict[j][i,:] for i,j in zip(list_id,text_id)])
+            frames = frames.view((-1, config.data.num_segments, 3) + frames.size()[-2:])
+            b,t,c,h,w = frames.size()
+            frames= frames.to(device).view(-1,c,h,w)
+            frame_emb = model.encode_image(frames)
+            frame_emb = frame_emb.view(b,t,-1)
+            texts = texts.view(-1, 77).to(device)
+            text_emb = model.encode_text(texts)  #.view(b, -1)
 
-            texts = train_data.classes[list_id]
-
-            images= images.to(device).view(-1,c,h,w)
-            texts = texts.to(device)
-            image_embedding = model_image(images)
-            image_embedding = image_embedding.view(b,t,-1)
-            image_embedding = fusion_model(image_embedding)
-
-            text_embedding = model_text(texts)
+            score_des_emb = model.encode_text(score_des)
+            item_des_emb = model.encode_text(item_des)
 
             logit_scale = model.logit_scale.exp()
-            logits_per_image, logits_per_text = create_logits(image_embedding,text_embedding,logit_scale)
 
-            ground_truth = torch.tensor(gen_label(list_id),dtype=image_embedding.dtype,device=device)
-            loss_imgs = loss_img(logits_per_image,ground_truth)
-            loss_texts = loss_txt(logits_per_text,ground_truth)
-            loss = (loss_imgs + loss_texts)/2
+            score_truth = torch.tensor(gen_label(score_label, score_des_emb.shape[0]), dtype=frame_emb.dtype, device=device) # (bs, bs)
+            item_truth = torch.tensor(gen_label(item_label, item_des_emb.shape[0]), dtype=frame_emb.dtype, device=device) # (bs, bs)
+            # score_truth = score_label.to(device)
+            # item_truth = item_label.to(device)
+            v2i_sim, v2s_sim, loss = fm_model(frame_emb, text_emb, item_des_emb, score_des_emb, logit_scale, item_truth, score_truth)
+
             total_loss += loss
             loss.backward()
 
@@ -133,25 +149,19 @@ def main():
                 clip.model.convert_weights(model)
         total_loss /= (kkk+1)
         if epoch % config.logging.eval_freq == 0:  # and epoch>0
-            prec1, prec3, prec5 = validate(epoch,val_loader, val_data.classes, device, model,fusion_model, config)
+            item_top1, item_top3, score_top1, score_top3 = validate(epoch, val_loader, item_des, score_des, device, model, fm_model, config)
 
-        if total_loss < loss_max:
-            step_count = 0
-            loss_max = total_loss
-            filename = "{}/best_loss.pt".format(working_dir)
-            epoch_saving(epoch, model, fusion_model, optimizer, filename)
-        else:
-            step_count += 1
-
-        is_best = prec1 > best_prec1
-        best_prec1 = max(prec1, best_prec1)
-        print('Testing: {:.3f}%/{:.3f}%, loss: {:.3f}'.format(prec1, best_prec1, total_loss))
-        print('Saving:')
+        is_best = score_top1 > bs1
+        bs1 = max(score_top1, bs1)
 
         if is_best:
-            best_prec5 = prec5
-            best_saving(working_dir, epoch, model, fusion_model, optimizer)
-    print('Best Testing: {}/{}'.format(best_prec1, best_prec5))
+            print('Saving Best:')
+            bi1, bi3, bs1, bs3 = item_top1, item_top3, score_top1, score_top3
+            best_saving(working_dir, epoch, model, fm_model, optimizer)
+            print('Best Testing: item: {:.2f}%, score: {:.2f}%'.format(bi1, bs1))
+
+    print('Best Test Item: Top1: {:.2f}%, Top3: {:.2f}%'.format(bi1, bi3))
+    print('Best Test Score: Top1: {:.2f}%, Top3: {:.2f}%'.format(bs1, bs3))
 
 if __name__ == '__main__':
     main()
